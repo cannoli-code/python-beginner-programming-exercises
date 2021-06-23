44061b708828a61cff3a57f5a2dc4c5b1512d15e
cannoli-code <mhernandezh@udd.cl>
2021-06-23 21:17:45 +0000

    python beginner

--- .learn/dist/cached_app.py
+++ .learn/dist/cached_app.py
@@ -1,4 +1,4 @@
 def execute_app():
-    # your code here
-    variables_are_cool = 2345 * 7323
-    print(variables_are_cool)
+    age = int(input('What is your age?\n'))
+    # CHANGE THE CODE BELOW TO ADD 10 TO AGE
+    print("Your age is: "+str(age +10))
